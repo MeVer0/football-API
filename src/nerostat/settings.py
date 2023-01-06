524142f885debae18033728985f0dda3acfa9b0b
Глеб Богатюк <mever.ya@gmail.com>
2023-01-06 12:55:03 +0300

    first commit

--- src/nerostat/settings.py
+++ src/nerostat/settings.py
@@ -1,10 +1,17 @@
+import json
+
 from pydantic import BaseSettings
+
+with open('config.json') as c:
+    config = json.load(c)
+
+
 
 
 class Settings(BaseSettings):
     server_host: str = "127.0.0.1"
     server_port: int = 8000
-    database_url: str
+    database_url: str = config.get('database_url')
 
 
 settings = Settings(
